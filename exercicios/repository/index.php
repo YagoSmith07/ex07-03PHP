6c44dae1d768d36082ffccc68923ed782f27c1a6
YagoSmith07 <yagosmith04@gmail.com>
2025-03-14 08:23:51 -0300

    primeiro exercício

---
+++ exercicios/repository/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio 1</title>
+</head>
+<body>
+<fieldset>
+        <form method = "POST" action = "calcular.php">
+        Insira O primeiro Numero
+        <input type = "text" name = "num1" size = "25">
+        <form method = "POST" action = "calcular.php">
+        Insira O segundo Numero
+        <input type = "text" name = "num2" size = "25">
+        <input type = "submit" name = "mensagemBNT" size = "50" value = "Resultado">
+        </form>
+    </fieldset>   
+    
+</body>
+</html>
